bcee177e122431652f766bc79a865156dcc1f116
Dmitry Vasilev <scythargon@gmail.com>
2017-12-30 20:53:59 +0500

    Add Django

--- webserver.py
+++ webserver.py
@@ -12,7 +12,7 @@ from flask import Flask, render_template, send_from_directory, request
 
 redis = redis_engine.StrictRedis(host="redis", port=6379, db=1, charset="utf-8", decode_responses=True)
 
-react_front = './front_react/build/'
+react_front = './frontend/build/'
 
 app = Flask(__name__, static_url_path="/static", static_folder=react_front)
 
